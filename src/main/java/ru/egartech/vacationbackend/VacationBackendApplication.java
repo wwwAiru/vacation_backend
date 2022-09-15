54a4cb073085060292c8e890e0dc6583f8d57df6
Dmitriy Lebedev <lediburit@mail.ru>
2022-09-15 14:34:31 +0300

    Initial commit feature/CU-2wu2ahu

---
+++ src/main/java/ru/egartech/vacationbackend/VacationBackendApplication.java
@@ -0,0 +1,11 @@
+package ru.egartech.vacationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VacationBackendApplication.class, args);
+    }
+}
